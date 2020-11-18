d4d956dddac05cfac936f691d4e307e37679e6d5
José Antonio González Morente <jgonzalezmorente@gmail.com>
2020-11-18 11:05:59 +0100

    Commit inicial

---
+++ src/app/pages/template/template.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { PaisService } from '../../services/pais.service';
+
+@Component({
+  selector: 'app-template',
+  templateUrl: './template.component.html',
+  styleUrls: ['./template.component.css']
+})
+export class TemplateComponent implements OnInit {
+
+  usuario = {
+    nombre: 'José Antonio',
+    apellido: 'González Morente',
+    correo: 'jgonzalezmorente@gmail.com',
+    pais: 'PRY',
+    genero: 'M'
+  };
+
+  paises: any[] = [];
+
+  constructor( private paisService: PaisService ) { }
+
+  ngOnInit(): void {
+
+    this.paisService.getPaises()
+    .subscribe( paises => {
+      this.paises = paises;
+      this.paises.unshift({
+        nombre: '[Seleccione País]',
+        codigo: ''
+      });
+    });
+  }
+
+  guardar( forma: NgForm ) {
+    console.log(forma);
+    if ( forma.invalid ) {
+      Object.values( forma.controls ).forEach( control => {
+        control.markAllAsTouched();
+      });
+      return;
+    }
+    console.log(forma.value);
+  }
+
+}
